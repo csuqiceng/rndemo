d9df13fcd4bd92fe892cdfe7457ae4b6bc87db50
yjcao <cyj19951028@gmail.com>
2021-07-21 19:55:02 +0800

    格式化

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { createBottomTabNavigator,createStackNavigator } from '@react-navigation/bottom-tabs';
-import {Image,DeviceEventEmitter} from 'react-native';
+import { createBottomTabNavigator, createStackNavigator } from '@react-navigation/bottom-tabs';
+import { Image, DeviceEventEmitter } from 'react-native';
 import ServicePage from "./service/servicePage";
 import MinePage from './personinfo/mine';
 import MallPage from './mall/mallPage';
@@ -8,13 +8,13 @@ import MallPage from './mall/mallPage';
 const Tab = createBottomTabNavigator();
 
 
-export default class MainPage extends React.Component{
-     constructor() {
-         super();
-         this.state={
-             showMarket: false
-         }
-     }
+export default class MainPage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            showMarket: false
+        }
+    }
 
     componentDidMount = () => {
         // 这里监听一个事件，如果需要显示商品推荐页面，则将showMarket置为true
@@ -30,55 +30,55 @@ export default class MainPage extends React.Component{
         this.subscribe && this.subscribe.remove();
     };
 
-     render() {
-         if (this.state.showMarket){
-             return null
-         }else{
-             return (
-                 <Tab.Navigator  screenOptions={{
-                     headerShown: false,
-                 }}>
-                     <Tab.Screen name="Home" component={ServicePage}  options={{
-                         tabBarLabel: '首页',
-                         tabBarIcon: ({ focused, tintColor }) => (
-                             <Image
-                                 source={focused ? require('../assets/images/tab_icon_home_sel.png') : require('../assets/images/tab_icon_home_sel.png')}
-                                 style={{ width: 26, height: 26, tintColor: tintColor }}
-                             />
-                         )
-                     }}/>
-                     <Tab.Screen name="Classify" component={MallPage} options={{
-                         // tabBarVisible:false,
-                         tabBarLabel: '分类',
-                         tabBarIcon: ({ focused, tintColor }) => (
-                             <Image
-                                 source={focused ? require('../assets/images/tab_icon_classify_nor.png') : require('../assets/images/tab_icon_classify_nor.png')}
-                                 style={{ width: 26, height: 26, tintColor: tintColor }}
-                             />
-                         )
-                     }}/>
-                     <Tab.Screen name="News" component={MinePage} options={{
-                         tabBarLabel: '新闻公告',
-                         tabBarIcon: ({ focused, tintColor }) => (
-                             <Image
-                                 source={focused ? require('../assets/images/tab_icon_news_nor.png') : require('../assets/images/tab_icon_news_nor.png')}
-                                 style={{ width: 26, height: 26, tintColor: tintColor }}
-                             />
-                         )
-                     }}/>
-                     <Tab.Screen name="MyInfo" component={MinePage} options={{
-                         tabBarLabel: '我的',
-                         tabBarIcon: ({ focused, tintColor }) => (
-                             <Image
-                                 source={focused ? require('../assets/images/tab_icon_my_nor.png') : require('../assets/images/tab_icon_my_nor.png')}
-                                 style={{ width: 26, height: 26, tintColor: tintColor }}
-                             />
-                         )
-                     }}/>
-                 </Tab.Navigator>
-             );
-         }
+    render() {
+        if (this.state.showMarket) {
+            return null
+        } else {
+            return (
+                <Tab.Navigator screenOptions={{
+                    headerShown: false,
+                }}>
+                    <Tab.Screen name="Home" component={ServicePage} options={{
+                        tabBarLabel: '首页',
+                        tabBarIcon: ({ focused, tintColor }) => (
+                            <Image
+                                source={focused ? require('../assets/images/tab_icon_home_sel.png') : require('../assets/images/tab_icon_home_sel.png')}
+                                style={{ width: 26, height: 26, tintColor: tintColor }}
+                            />
+                        )
+                    }} />
+                    <Tab.Screen name="Classify" component={MallPage} options={{
+                        // tabBarVisible:false,
+                        tabBarLabel: '分类',
+                        tabBarIcon: ({ focused, tintColor }) => (
+                            <Image
+                                source={focused ? require('../assets/images/tab_icon_classify_nor.png') : require('../assets/images/tab_icon_classify_nor.png')}
+                                style={{ width: 26, height: 26, tintColor: tintColor }}
+                            />
+                        )
+                    }} />
+                    <Tab.Screen name="News" component={MinePage} options={{
+                        tabBarLabel: '新闻公告',
+                        tabBarIcon: ({ focused, tintColor }) => (
+                            <Image
+                                source={focused ? require('../assets/images/tab_icon_news_nor.png') : require('../assets/images/tab_icon_news_nor.png')}
+                                style={{ width: 26, height: 26, tintColor: tintColor }}
+                            />
+                        )
+                    }} />
+                    <Tab.Screen name="MyInfo" component={MinePage} options={{
+                        tabBarLabel: '我的',
+                        tabBarIcon: ({ focused, tintColor }) => (
+                            <Image
+                                source={focused ? require('../assets/images/tab_icon_my_nor.png') : require('../assets/images/tab_icon_my_nor.png')}
+                                style={{ width: 26, height: 26, tintColor: tintColor }}
+                            />
+                        )
+                    }} />
+                </Tab.Navigator>
+            );
+        }
 
-     }
+    }
 
 }
